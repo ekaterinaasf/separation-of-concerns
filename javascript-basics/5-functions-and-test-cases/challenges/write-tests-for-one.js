bea2e9c09aaef6787da59ed08cebea82b2af1c9b
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-02-01 17:29:53 +0100

    NaN-test

--- javascript-basics/5-functions-and-test-cases/challenges/write-tests-for-one.js
+++ javascript-basics/5-functions-and-test-cases/challenges/write-tests-for-one.js
@@ -4,9 +4,10 @@ const testCases = [
   { name: 'first', args: [], expected: '(false) 1, 1 (false)' },
   { name: 'second', args: [], expected: '(true) 1, a (false)' },
   { name: 'third', args: [], expected: '(false) null, null (false)' },
-  { name: 'fourth', args: [NaN, NaN], expected: _ },
-  { name: 'fifth', args: [], expected: _ },
+  { name: 'fourth', args: [], expected: '(false) NaN, NaN (false)' },
+  { name: 'fifth', args: [NaN, NaN], expected: _ },
   { name: 'sixth', args: [], expected: _ },
+  { name: 'seventh', args: [], expected: _ },
 
   // for the last two, find different arguments with the same expected value
   { name: 'seventh', args: [], expected: '(true) 1, 1 (true)' },
